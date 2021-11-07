bafe534e89db4152ea53de30cb805acd83b8e799
aqua5201314 <aqua5201314@outlook.com>
2021-11-06 21:10:20 -0700

    添加日志类

---
+++ logger.cc
@@ -0,0 +1,40 @@
+#include "logger.h"
+
+#include <iostream>
+
+// 获取日志唯一的实例对象
+Logger &Logger::instance()
+{
+    static Logger logger;
+    return logger;
+}
+
+// 设置日志级别
+void Logger::setLogLevel(int level)
+{
+    logLevel_ = level;
+}
+
+// 写日志  [级别信息] time : msg
+void Logger::log(std::string msg)
+{
+    if (logLevel_ == INFO)
+    {
+        std::cout << "[INFO]";
+    }
+    else if (logLevel_ == ERROR)
+    {
+        std::cout << "[ERROR]";
+    }
+    else if (logLevel_ == FATAL)
+    {
+        std::cout << "FATAL";
+    }
+    else if (logLevel_ == DEBUG)
+    {
+        std::cout << "DEBUG";
+    }
+
+    // 打印时间
+    std::cout<<"print time"<<" : "<<msg<<std::endl;
+}
