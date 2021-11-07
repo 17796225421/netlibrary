c9894987487831f6ed860975852832ca669208bb
aqua5201314 <aqua5201314@outlook.com>
2021-11-07 04:01:35 -0800

    添加Channel类

---
+++ EventLoop.h
@@ -0,0 +1,8 @@
+#pragma
+
+// 事件循环类  主要包含了两个大模块 Channel  Poller(epoll的抽象)
+class EventLoop
+{
+public:
+private:
+};
--- logger.h
+++ logger.h
@@ -5,14 +5,14 @@
 #include "noncopyable.h"
 
 // LOG_INFO("%s %d",arg1,arg2)
-#define LOG_INFO(logmsgFormat, ...)                      \
-    do                                                   \
-    {                                                    \
-        Logger &logger = Logger::instance();             \
-        logger.setLogLevel(INFO);                        \
-        char buf[1024] = {0};                            \
-        snprintf(buf, 1024, logmsgFormat, ##_VA_ARGS__); \
-        logger.log(buf);                                 \
+#define LOG_INFO(logmsgFormat, ...)                       \
+    do                                                    \
+    {                                                     \
+        Logger &logger = Logger::instance();              \
+        logger.setLogLevel(INFO);                         \
+        char buf[1024] = {0};                             \
+        snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
+        logger.log(buf);                                  \
     } while (0)
 
 #define LOG_ERROR(logmsgFormat, ...)                     \
@@ -21,7 +21,7 @@
         Logger &logger = Logger::instance();             \
         logger.setLogLevel(ERROR);                       \
         char buf[1024] = {0};                            \
-        snprintf(buf, 1024, logmsgFormat, ##_VA_ARGS__); \
+        snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
         logger.log(buf);                                 \
     } while (0)
 
@@ -31,7 +31,7 @@
         Logger &logger = Logger::instance();             \
         logger.setLogLevel(FATAL);                       \
         char buf[1024] = {0};                            \
-        snprintf(buf, 1024, logmsgFormat, ##_VA_ARGS__); \
+        snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
         logger.log(buf);                                 \
     } while (0)
 
@@ -42,7 +42,7 @@
         Logger &logger = Logger::instance();             \
         logger.setLogLevel(DEBUG);                       \
         char buf[1024] = {0};                            \
-        snprintf(buf, 1024, logmsgFormat, ##_VA_ARGS__); \
+        snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
         logger.log(buf);                                 \
     } while (0)
 #else
