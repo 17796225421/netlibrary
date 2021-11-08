f20e8d2e30f862fde5ef46b089babc0887d90f84
aqua5201314 <aqua5201314@outlook.com>
2021-11-08 03:57:15 -0800

    添加Socket类

--- InetAddress.h
+++ InetAddress.h
@@ -11,12 +11,13 @@ public:
     explicit InetAddress(uint16_t port, std::string ip = "127.0.0.1");
     explicit InetAddress(const sockaddr_in &addr)
         : addr_(addr) {}
-    
-    std::string toIp()const;
-    std::string toIpPort()const;
-    uint16_t toPort()const;
 
-    const sockaddr_in*getSockAddr()const{return &addr_;}
+    std::string toIp() const;
+    std::string toIpPort() const;
+    uint16_t toPort() const;
+
+    const sockaddr_in *getSockAddr() const { return &addr_; }
+    void setSockAddr(const sockaddr_in &addr) { addr_ = addr; }
 
 private:
     sockaddr_in addr_;
